aa7ba250fe871d1419621327be5b793b9067af36
Azamino28ki <miyamizu328@outlook.jp>
2024-06-01 17:58:07 +0900

    user_data修正

--- user_data.sh
+++ user_data.sh
@@ -5,12 +5,12 @@ wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenki
 rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
 yum upgrade
 yum install java-17-amazon-corretto-devel jenkins git -y
-wget https://raw.githubusercontent.com/OrangeJuice652/FlutterSetupOnLinux/main/flutter_setup_on_linux.sh -P ~jenkins/Downloads
-wget https://raw.githubusercontent.com/OrangeJuice652/FlutterSetupOnLinux/main/jenkins_plugin.sh -P ~jenkins/Downloads
-chown -R jenkins:jenkins ~jenkins/Downloads/
-chmod u+x ~jenkins/Downloads/flutter_setup_on_linux.sh
-chmod u+x ~jenkins/Downloads/jenkins_plugin.sh
+sudo wget https://raw.githubusercontent.com/OrangeJuice652/FlutterSetupOnLinux/main/flutter_setup_on_linux.sh -P ~jenkins/Downloads
+sudo wget https://raw.githubusercontent.com/OrangeJuice652/FlutterSetupOnLinux/main/jenkins_plugin.sh -P ~jenkins/Downloads
+sudo chown -R jenkins:jenkins ~jenkins/Downloads/
+sudo chmod u+x ~jenkins/Downloads/flutter_setup_on_linux.sh
+sudo chmod u+x ~jenkins/Downloads/jenkins_plugin.sh
 sudo -u jenkins ~jenkins/Downloads/flutter_setup_on_linux.sh
 sudo -u jenkins ~jenkins/Downloads/jenkins_plugin.sh
-systemctl enable jenkins
-systemctl start jenkin
+sudo systemctl enable jenkins
+sudo systemctl start jenkin
