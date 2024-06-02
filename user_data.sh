c80ebfbf58ff26ff1d8e5ec08f9a480580ce0807
Azamino28ki <miyamizu328@outlook.jp>
2024-06-02 09:26:09 +0900

    user_data更新

--- user_data.sh
+++ user_data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # setup Jenkins on EC2
-yum update -y
+sudo yum update -y
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
-yum upgrade
-yum install java-17-amazon-corretto-devel jenkins git -y
+sudo yum upgrade
+sudo yum install java-17-amazon-corretto-devel jenkins git -y
 sudo wget https://raw.githubusercontent.com/OrangeJuice652/FlutterSetupOnLinux/main/flutter_setup_on_linux.sh -P ~jenkins/Downloads
 sudo wget https://raw.githubusercontent.com/OrangeJuice652/FlutterSetupOnLinux/main/jenkins_plugin.sh -P ~jenkins/Downloads
 sudo chown -R jenkins:jenkins ~jenkins/Downloads/
