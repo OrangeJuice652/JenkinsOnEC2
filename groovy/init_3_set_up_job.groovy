3ecaaff280c5e50728b524e447c06a2c449fbfdf
Azamino28ki <miyamizu328@outlook.jp>
2024-06-09 17:14:59 +0900

    set_up_job.groovy更新

--- groovy/init_3_set_up_job.groovy
+++ groovy/init_3_set_up_job.groovy
@@ -3,6 +3,6 @@ import jenkins.model.*
 def instance  = Jenkins.getInstance()
 
 def jobName = "FulutterBuildPipline"
-def configXml = new File("~/jenkins/FlutterBuildPipline.xml").text
+def configXml = new File("~jenkins/FlutterBuildPipline.xml").text
 def xmlStream = new ByteArrayInputStream( configXml.getBytes() )
 instance.createProjectFromXML(jobName, xmlStream)
